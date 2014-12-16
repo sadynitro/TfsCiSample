0416dc3e719ad3002950b1f79272bdda37ef8a15
貞松 政史 <m-sadamatsu@rhizome-e.com>
2014-12-16 12:54:51 +0900

    first commit

---
+++ SampleApp01/SampleApp01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleApp01.MvcApplication" Language="C#" %>
---
+++ SampleApp01/SampleApp01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleApp01.Startup))]
+namespace SampleApp01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
