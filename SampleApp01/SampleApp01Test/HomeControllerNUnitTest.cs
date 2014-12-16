fe6a57ac0b1718b479a757d617c3be7908d2438a
貞松 政史 <m-sadamatsu@rhizome-e.com>
2014-12-16 14:02:20 +0900

    テストプロジェクトを追加

---
+++ SampleApp01/SampleApp01Test/HomeControllerNUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Core;
+using NUnit.Framework;
+
+namespace SampleApp01Test
+{
+  [TestFixture]
+  class HomeControllerNUnitTest
+  {
+    [Test]
+    public void IndexTest()
+    {
+      var expect = 0;
+      var actual = 0;
+      Assert.AreEqual(expect, actual);
+    }
+  }
+}
---
+++ SampleApp01/SampleApp01Test/HomeControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleApp01Test
+{
+  [TestClass]
+  public class HomeControllerTest
+  {
+    [TestMethod]
+    public void Index()
+    {
+      var expect = 0;
+      var actual = 0;
+      Assert.AreEqual(expect, actual);
+    }
+  }
+}
